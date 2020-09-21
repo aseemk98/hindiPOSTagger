8de42f9fb67092784355824f979e63351af3ba16
Atharva Bhagwat <atharva.bhagwat42@gmail.com>
2020-09-22 00:02:07 +0530

    Added MAJOR CHANGES

--- app.py
+++ app.py
@@ -8,31 +8,26 @@ import re
 from nltk.tag import tnt
 from indicnlp.tokenize import indic_tokenize
 import pickle
-from flask import jsonify
-app = Flask(__name__)             
+from flask import jsonify, render_template
+app = Flask(__name__)      
 
 @app.route("/")                   
 def hello():                      
-    return "Hello World!"         
-
-
+    return render_template('index.html')
 
 @app.route("/getpos",methods=['POST'])
 def getpos():
-    #req_data = request.get_json()
-    
     text = request.form['input_text']
     tokenized_text = indic_tokenize.trivial_tokenize(text)
     with open("./postagger.pkl",'rb') as f:
         pos_tagger = pickle.load(f)
     
     output = pos_tagger.tag(tokenized_text)
-    op_dict = {}
-    op_dict['output'] = output
-    
-    return jsonify(op_dict)
-    
-
+    print(output)
+    # op_dict = {}
+    # op_dict['output'] = output
+    return render_template('result.html',data = output)
 
 if __name__ == "__main__":        # on running python app.py
-    app.run()  
+    app.run(debug=True)
+    # ३९ गेंदों में दो चौकों और एक छक्के की मदद से ३४ रन बनाने वाले परोरे अंत तक आउट नहीं हुए।
